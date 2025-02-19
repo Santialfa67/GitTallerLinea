bc2cc1d6449c8f4cf6a3bc59b0df56cba41d83b4
Santiago Alarcón <santialfa67@gmail.com>
2025-02-19 14:19:16 -0500

    Nueva linea para resolver

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Conocimiento acerca del uso de GitHub, programando en Ja");
+        System.out.printf("Merge");
         System.out.printf("Primer Commmit");
+        System.out.printf("Merge");
 
+
+        System.out.printf("Vamos a resolver un conflicto");
     }
 }
