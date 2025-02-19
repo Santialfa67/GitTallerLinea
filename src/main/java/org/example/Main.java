4767c8b6696336d9b977075c9c0582f26e8e57ef
Santiago Alarcón <santialfa67@gmail.com>
2025-02-19 14:48:55 -0500

    Limpieza

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.printf("Conocimiento acerca del uso de GitHub, programando en Ja");
-        System.out.printf("Merge");
-        System.out.printf("Primer Commmit");
-        System.out.printf("Primer Commmit");
-        System.out.printf("Merge");
-
-
-        System.out.printf("Vamos a resolver un conflicto");
     }
 }
