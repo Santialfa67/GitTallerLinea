a0c377678515db0f438542d3aa34636d67b89695
Santiago Alarcón <santialfa67@gmail.com>
2025-02-18 20:40:24 -0500

    Demostracion de commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Conocimiento acerca del uso de GitHub, programando en Ja");
+        System.out.printf("Primer Commmit");
 
     }
 }
